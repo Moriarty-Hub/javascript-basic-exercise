35740356f22da9a48cda5af44fe155cc6fecf6bc
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-07-26 17:47:14 +0800

    Finish exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,22 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  const list = {
+    value: array[0],
+    next: null,
+  };
+  let current = list;
+  for (let i = 1; i < array.length; i += 1) {
+    current.next = {};
+    current = current.next;
+    current.value = array[i];
+    current.next = null;
+  }
+  return list;
 }
