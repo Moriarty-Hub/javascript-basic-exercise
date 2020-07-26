c1b673a9a3b5e3df65929548bcd176f88b006e4c
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-07-26 16:04:28 +0800

    Finish exercise_02

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,17 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (string === null || string === undefined) {
+    return 0;
+  }
+  if (prediction === undefined) {
+    return string.length;
+  }
+  let length = 0;
+  for (let i = 0; i < string.length; i += 1) {
+    if (prediction(string[i])) {
+      length += 1;
+    }
+  }
+  return length;
 }
