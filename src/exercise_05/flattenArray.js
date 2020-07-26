2c81f46bef5186c93eb2f0d83697f1f42e5c62f3
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-07-26 20:15:37 +0800

    Finish exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,16 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
+  let result = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (Array.isArray(array[i])) {
+      result = result.concat(array[i]);
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
